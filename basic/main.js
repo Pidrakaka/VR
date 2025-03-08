c5b443a5289842ba658f206cf363df89eda6ca8d
Pidrakaka <nursatturlykylyshev@gmail.com>
2025-03-08 15:34:36 +0100

    Краткое описание изменений

--- basic/main.js
+++ basic/main.js
@@ -6,7 +6,7 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const amblight = new THREE.AmbientLight( 0x404040, 10 ); // soft white light
+const amblight = new THREE.AmbientLight( 0x404040, 25 ); // soft white light
 scene.add( amblight );
 
 //--------
@@ -26,19 +26,62 @@ document.body.appendChild(renderer.domElement);
 document.body.appendChild(VRButton.createButton(renderer));
 
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 10;
 
 const loader = new GLTFLoader();
 
 // Load a glTF resource
+// loader.load(
+//     // resource URL
+//     'models/zabka/scene.gltf',
+//     // called when the resource is loaded
+//     function ( gltf ) {
+//
+//         gltf.scene.scale.set(10, 10, 10);
+//         scene.add( gltf.scene );
+//     },
+//     // called while loading is progressing
+//     function ( xhr ) {
+//
+//         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+//     },
+//     // called when loading has errors
+//     function ( error ) {
+//
+//         console.log( 'An error happened' );
+//
+//     }
+// );
+
+// loader.load(
+//     // resource URL
+//     'models/kfc/scene.gltf',
+//     // called when the resource is loaded
+//     function ( gltf ) {
+//
+//         gltf.scene.scale.set(10, 10, 10);
+//         scene.add( gltf.scene );
+//     },
+//     // called while loading is progressing
+//     function ( xhr ) {
+//
+//         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+//     },
+//     // called when loading has errors
+//     function ( error ) {
+//
+//         console.log( 'An error happened' );
+//
+//     }
+// );
 loader.load(
     // resource URL
-    'models/zabka/scene.gltf',
+    'models/CV/CV2.gltf',
     // called when the resource is loaded
     function ( gltf ) {
 
@@ -58,27 +101,6 @@ loader.load(
     }
 );
 
-loader.load(
-    // resource URL
-    'models/kfc/scene.gltf',
-    // called when the resource is loaded
-    function ( gltf ) {
-
-        gltf.scene.scale.set(10, 10, 10);
-        scene.add( gltf.scene );
-    },
-    // called while loading is progressing
-    function ( xhr ) {
-
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-    },
-    // called when loading has errors
-    function ( error ) {
-
-        console.log( 'An error happened' );
-
-    }
-);
 
 const orbitControls = new OrbitControls(camera, renderer.domElement);
 
